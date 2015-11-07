1c487a720ed748095c82361cf64b99a5b81e9593
David Rubert <david.rubert@gmail.com>
2015-11-07 18:15:47 +0100

    more slides

--- presentation/deck.jsx
+++ presentation/deck.jsx
@@ -12,7 +12,13 @@ const images = {
     gmail_logo: require("./images/gmail-logo.png"),
     swissarmy: require("./images/swissarmy.jpg"),
     identity01: require("./images/identity01.png"),
-    templates01: require("./images/templates01.png")
+    labs: require("./images/labs.png"),
+    labs01: require("./images/labs01.png"),
+    labs02: require("./images/labs02.png"),
+    labs03: require("./images/labs03.png"),
+    templatesLogo: require("./images/templates-logo.jpg"),
+    templates01: require("./images/templates01.png"),
+    templates02: require("./images/templates02.png")
 };
 
 export default class extends React.Component {
@@ -51,71 +57,120 @@ export default class extends React.Component {
         </Slide>
 
         <Slide transition={["slide"]} bgImage={images.identity01.replace("/", "")} bgDarken={0.1}>
-            <Heading size={2} caps fit textColor="grey">
-                Ejemplo de gestión de identidades
+            <Heading margin="4em 0 0 0" size={2} caps fit textColor="brown">
+                Gestión de identidades
             </Heading>
-            <Text textColor="white">Pulsa la flecha &rarr;  para continuar...</Text>
+            <Text textColor="black">Pulsa la flecha &rarr;  para continuar...</Text>
         </Slide>
 
         <Slide transition={["slide"]} bgImage={images.templates01.replace("/", "")} bgDarken={0.1}>
-            <Heading size={2} caps fit textColor="brown">
-                Ejemplo de gestión de plantillas
+            <Heading margin="4em 0 0 0" size={2} caps fit textColor="#E67819">
+                Gestión de plantillas
             </Heading>
             <Text textColor="grey">Pulsa la flecha &rarr;  para continuar...</Text>
         </Slide>
 
-        <Slide transition={["slide"]} bgColor="white">
+        <Slide transition={["slide"]} bgColor="#8EDE9A">
             <Heading size={2} caps fit textColor="black">
                 Ejemplo de funcionamiento
             </Heading>
 
-            <Text textSize="2em" padding="1em" bgColor="#EFEFEF">Vamos a hacer una demostración, enviadme 3 o 4 de vosotros/as un correo a la dirección de correo <Link href="mailto:curs-gmail@uji.es">curs-gmail@uji.es</Link> que véis más abajo preguntando por "el estado de vuestra solicitud de admisión al curso".</Text>
+            <Text textSize="2em" padding="1em" bgColor="#7ABF85">Vamos a hacer una demostración, enviadme 3 o 4 de vosotros/as un correo a la dirección de correo que véis más abajo preguntando por "el estado de vuestra solicitud de admisión al curso".</Text>
+            <Text><Link href="mailto:curs-gmail@uji.es">curs-gmail@uji.es</Link></Text>
+        </Slide>
+
+        <Slide transition={["slide"]} bgColor="red">
+            <Heading size={2} caps fit textColor="white">
+                Direcciones de correo impersonales
+            </Heading>
+            <Text>¿De dónde salen y cómo puedo solicitar una?</Text>
+            <List textColor="yellow" bgColor="brown" padding="1em" margin="0 5em">
+              <ListItem>Alias de correo</ListItem>
+              <ListItem>Lista de correo</ListItem>
+            </List>
+            <Text>SPI en e-ujier</Text>
         </Slide>
 
         <Slide transition={["zoom", "fade"]} bgColor="primary">
-          <Heading caps fit>Flexible Layouts</Heading>
+          <Heading caps fit>Plantillas prediseñadas</Heading>
           <Layout>
             <Fill>
               <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                Left
+                1. Activación
               </Heading>
             </Fill>
             <Fill>
               <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                Right
+                2. Uso
               </Heading>
             </Fill>
           </Layout>
         </Slide>
+
+        <Slide transition={["slide"]} bgImage={images.labs.replace("/", "")} bgDarken="0.6">
+            <Appear fid="1">
+              <Heading size={1} caps fit textColor="white">
+                Utilidad
+              </Heading>
+            </Appear>
+            <List textColor="yellow">
+              <ListItem><Appear fid="2">Funciones adicionales, no activadas por defecto</Appear></ListItem>
+              <ListItem><Appear fid="3">Si mucha gente las utiliza, las activan para todos</Appear></ListItem>
+              <ListItem><Appear fid="4">Las plantillas de respuesta predefinida están incluidas aquí</Appear></ListItem>
+            </List>
+            <Text textColor="white">Pulsa la flecha &rarr;  para continuar...</Text>
+        </Slide>
+
+        <Slide transition={["slide"]} bgImage={images.labs01.replace("/", "")} bgDarken={0.1} bgColor="white">
+            <Text margin="12em 0 0 0" cap fit textColor="brown">Pulsa la flecha &rarr;  para continuar...</Text>
+        </Slide>
+
+        <Slide transition={["slide"]} bgImage={images.labs02.replace("/", "")} bgDarken={0.1}>
+            <Text margin="12em 0 0 0" cap fit textColor="#697023">Pulsa la flecha &rarr;  para continuar...</Text>
+        </Slide>
+
+        <Slide transition={["slide"]} bgImage={images.labs03.replace("/", "")} bgDarken={0.1}>
+            <Text margin="12em 0 0 0" cap fit textColor="#952044">Pulsa la flecha &rarr;  para continuar...</Text>
+        </Slide>
+
         <Slide transition={["slide", "spin"]} bgColor="primary">
-          <Heading caps fit size={1} textColor="tertiary">
-            Smooth
-          </Heading>
-          <Heading caps fit size={1} textColor="secondary">
-            Combinable Transitions
-          </Heading>
+            <Heading size={2} caps fit textColor="tertiary">
+                Ejercicio
+            </Heading>
+
+            <Text textSize="2em" padding="1em" bgColor="#F8D12C">Entra en tu cuenta de correo electrónico de Gmail de la UJI, accede a las opciones de configuración, pestaña de Labs, y activa las Respuestas Predefinidas. ¿Hay alguna otra utilidad que consideres interesante?</Text>
+            <Text><Link href="http://gmail.uji.es">http://gmail.uji.es</Link></Text>
         </Slide>
-        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-          <List>
-            <ListItem><Appear fid="1">Inline style based theme system</Appear></ListItem>
-            <ListItem><Appear fid="2">Autofit text</Appear></ListItem>
-            <ListItem><Appear fid="3">Flexbox layout system</Appear></ListItem>
-            <ListItem><Appear fid="4">React-Router navigation</Appear></ListItem>
-            <ListItem><Appear fid="5">PDF export</Appear></ListItem>
-            <ListItem><Appear fid="6">And...</Appear></ListItem>
-          </List>
+
+        <Slide transition={["slide"]} bgImage={images.templatesLogo.replace("/", "")} bgDarken={0.1}>
+            <Text margin="12em 0 0 0" cap fit textColor="#697023">Pulsa la flecha &rarr;  para continuar...</Text>
         </Slide>
-        <Slide transition={["slide"]} bgColor="primary">
-          <Heading size={1} caps fit textColor="tertiary">
-            Your presentations are interactive
-          </Heading>
+
+        <Slide transition={["zoom", "fade"]} bgColor="#F2E9D9">
+          <Heading caps fit textColor="brown">Gestión de Plantillas</Heading>
+          <Layout>
+            <Fill>
+              <Heading size={4} caps textColor="secondary" bgColor="#D8D6B3" margin={10}>
+                Insertar
+              </Heading>
+            </Fill>
+            <Fill>
+              <Heading size={4} caps textColor="secondary" bgColor="#C6B893" margin={10}>
+                Guardar
+              </Heading>
+            </Fill>
+            <Fill>
+              <Heading size={4} caps textColor="secondary" bgColor="#847D7C" margin={10}>
+                Borrar
+              </Heading>
+            </Fill>
+          </Layout>
         </Slide>
-        <Slide transition={["spin", "slide"]} bgColor="tertiary">
-          <Heading size={1} caps fit lineHeight={1.5} textColor="primary">
-            Made with love in Seattle by
-          </Heading>
-          <Link href="http://www.formidablelabs.com"><Image width="100%" src={images.logo}/></Link>
+
+        <Slide transition={["slide"]} bgImage={images.templates02.replace("/", "")} bgDarken={0.1}>
+            <Text margin="12em 0 0 0" textColor="grey">Pulsa la flecha &rarr;  para continuar...</Text>
         </Slide>
+
       </Deck>
     );
   }
