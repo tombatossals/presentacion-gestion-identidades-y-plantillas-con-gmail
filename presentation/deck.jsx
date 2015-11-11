b131070081fb5f0c1b1bf8e7e7d5f843e41218da
David Rubert <david.rubert@gmail.com>
2015-11-11 19:32:34 +0100

    more

--- presentation/deck.jsx
+++ presentation/deck.jsx
@@ -10,7 +10,9 @@ const images = {
     batman: require("./images/batman.jpg"),
     bruce: require("./images/bruce.jpg"),
     identity01: require("./images/identity01.png"),
+    identity02: require("./images/identity02.png"),
     signature01: require("./images/signature01.png"),
+    signaturelogo: require("./images/signature_logo.jpg"),
     automatic01: require("./images/automatic01.png"),
     config01: require("./images/config01.png"),
 };
@@ -52,17 +54,17 @@ export default class extends React.Component {
           <Text>Una misma persona puede tener múltiples direcciones de correo diferentes.</Text>
         </Slide>
 
-        <Slide transition={["slide"]} bgColor="red">
+        <Slide transition={["slide"]} bgColor="brown">
             <Heading size={2} caps fit textColor="white">
                 Direcciones de correo impersonales
             </Heading>
             <Text>¿De dónde salen y cómo puedo solicitar una?</Text>
             <List textColor="yellow" bgColor="brown" padding="1em" margin="0 5em">
               <ListItem>Alias de correo. Ej: se@uji.es</ListItem>
-              <ListItem>Alias extendido. Ej: david.rubert@uji.es</ListItem>
+              <ListItem>Alias extendido. Ej: david.rubert@uji.es (<Link href="https://e-ujier.uji.es/pls/www/!gri_ass.spi020102?p_proc=356&p_tema=12">enlace</Link>)</ListItem>
               <ListItem>Lista de correo. Ej: pas@uji.es</ListItem>
             </List>
-            <Text>SPI en e-ujier</Text>
+            <Text><Link href="https://e-ujier.uji.es/pls/www/gri_ass.spi020101">SPI en e-ujier</Link></Text>
         </Slide>
 
         <Slide transition={["slide", "spin"]} bgColor="primary">
@@ -74,6 +76,20 @@ export default class extends React.Component {
             <Text textSize="2em">Os he añadido a todos/as al siguiente alias temporal <Link href="mailto:curs-gmail@uji.es">curs-gmail@uji.es</Link></Text>
         </Slide>
 
+        <Slide transition={["slide"]} bgColor="#7D9796">
+            <Heading size={2} caps fit textColor="white">
+                Añadir nueva identidad
+            </Heading>
+            <Text>Veamos cómo podemos añadir una nueva identidad a nuestra lista de direcciones</Text>
+            <List textColor="yellow" padding="1em" margin="0 5em">
+              <ListItem>Accedemos a configuración/cuentas</ListItem>
+              <ListItem>Pulsamos añadir identidad</ListItem>
+              <ListItem>Introducimos el nombre y correo del álias</ListItem>
+              <ListItem>Nos enviará un e-mail para confirmar</ListItem>
+            </List>
+            <Text>Ya hemos terminado el proceso, lo vamos a ver con un ejemplo</Text>
+        </Slide>
+
         <Slide transition={["slide"]} bgImage={images.config01.replace("/", "")} bgDarken={0.1} bgColor="white">
             <Text margin="12em 0 0 0" cap fit textColor="brown">Pulsa la flecha &rarr;  para continuar...</Text>
         </Slide>
@@ -82,18 +98,31 @@ export default class extends React.Component {
             <Text margin="12em 0 0 0" cap fit textColor="brown">Pulsa la flecha &rarr;  para continuar...</Text>
         </Slide>
 
+        <Slide transition={["slide"]} bgImage={images.identity02.replace("/", "")} bgDarken={0.1} bgColor="white">
+            <Text margin="12em 0 0 0" cap fit textColor="brown">Pulsa la flecha &rarr;  para continuar...</Text>
+        </Slide>
+
         <Slide transition={["slide", "spin"]} bgColor="#AF806E">
-            <Heading size={2} caps fit textColor="tertiary">
-                Identidades
+            <Heading size={4} caps fit textColor="tertiary">
+                Ejercicio
             </Heading>
 
             <List textColor="yellow" bgColor="#AF806E" padding="1em" margin="0">
               <ListItem>Accede a la configuración de cuentas.</ListItem>
               <ListItem>Comprueba qué identidades tienes configuradas.</ListItem>
               <ListItem>Establece como predeterminada la dirección que prefieras.</ListItem>
+              <ListItem>Añade tu nueva identidad "curs-gmail@uji.es".</ListItem>
               <ListItem>Elimina aquellas identidades que ya no necesites</ListItem>
             </List>
             <Text>No tengas miedo en probar, ya que cualquier fallo que cometamos puede solucionarse.</Text>
+        </Slide>
+
+        <Slide transition={["slide"]} bgImage={images.signaturelogo.replace("/", "")} bgDarken={0.1}>
+          <Heading textColor="green" size={2} caps fit>
+              Firmas
+          </Heading>
+
+          <Text cap fit textColor="brown">Pulsa la flecha &rarr;  para continuar...</Text>
         </Slide>
 
         <Slide transition={["slide", "spin"]} bgColor="#C7A0AC">
